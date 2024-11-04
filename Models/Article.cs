b11cad736ae85fe1ae34f4dc5a828c2f9a722437
Nguyễn Trọng Hoàng <hoangnguyen30092003@gmail.com>
2024-11-05 00:46:38 +0700

    Tao trang CRUD, Paging, trong Article

--- Models/Article.cs
+++ Models/Article.cs
@@ -5,20 +5,22 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace c_.git.razorweb.Models
-{
+namespace c_.git.razorweb.Models;
+
   public class Article
   {
     [Key]
     public int Id {get;set;}
-    [MaxLength(255)]
-    [Required]
+    [StringLength(255, MinimumLength = 5, ErrorMessage = "Tieu de phai tu {2} den {1} ki tu")]
+    [Required(ErrorMessage ="{0} phai nhap")]
     [Column(TypeName ="nvarchar")]
+    [Display(Name ="Ten")]
     public string Title {get;set;} = string.Empty;
     [DataType(DataType.Date)]
     [Required]
+    [Display(Name ="Thoi gian")]
     public DateTime Created {get;set;}
     [Column(TypeName ="ntext")]
+    [Display(Name ="Noi dung")]
     public string Content {get;set;} =string.Empty;
   }
-}
